71dfa511b550e5e6d14a29380dd5586ad4d966d9
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-09-10 23:33:15 +0530

    KMP and min char for palindromic string

--- Strings/KmpPatternSearch.cpp
+++ Strings/KmpPatternSearch.cpp
@@ -4,79 +4,70 @@
   
 void computeLPSArray(char* pat, int M, int* lps); 
   
-// Prints occurrences of txt[] in pat[] 
+// Prints occurrences of pat[] in txt[] 
+// lps[i] = the longest proper prefix of pat[0..i] which is also a suffix of pat[0..i]
+
 void KMPSearch(char* pat, char* txt) 
 { 
     int M = strlen(pat); 
     int N = strlen(txt); 
   
-    // create lps[] that will hold the longest prefix suffix 
-    // values for pattern 
     int lps[M]; 
   
     // Preprocess the pattern (calculate lps[] array) 
     computeLPSArray(pat, M, lps); 
   
-    int i = 0; // index for txt[] 
-    int j = 0; // index for pat[] 
-    while (i < N) { 
-        if (pat[j] == txt[i]) { 
-            j++; 
-            i++; 
-        } 
-  
-        if (j == M) { 
-            printf("Found pattern at index %d ", i - j); 
-            j = lps[j - 1]; 
-        } 
-  
-        // mismatch after j matches 
-        else if (i < N && pat[j] != txt[i]) { 
+    int i = 0;
+    int j = 0;
+
+    while(i < N) {
+        if(pat[j] == txt[i]) {
+            i++;
+            j++;
+        }
+        if(j == M) {
+            cout<<"Pattern found at "<< i - j;
+            j = lps[j - 1];
+        }
+        // mismatch after j matches
+        else if(i < N && pat[j] != txt[i]) {
             // Do not match lps[0..lps[j-1]] characters, 
-            // they will match anyway 
-            if (j != 0) 
-                j = lps[j - 1]; 
-            else
-                i = i + 1; 
-        } 
-    } 
+			// they will match anyway 
+            if(j != 0) {
+                j = lps[j - 1];
+            }
+            else {
+                i++;
+            }
+        }
+    }
 } 
   
 // Fills lps[] for given patttern pat[0..M-1] 
-void computeLPSArray(char* pat, int M, int* lps) 
-{ 
-    // length of the previous longest prefix suffix 
+void computeLPSArray (char *pat, int M, int *lps) {
     int len = 0; 
-  
-    lps[0] = 0; // lps[0] is always 0 
-  
-    // the loop calculates lps[i] for i = 1 to M-1 
+    lps[0] = 0; // lps[0] is always 0  
     int i = 1; 
+
     while (i < M) { 
         if (pat[i] == pat[len]) { 
             len++; 
             lps[i] = len; 
             i++; 
         } 
-        else // (pat[i] != pat[len]) 
+        else
         { 
-            // This is tricky. Consider the example. 
-            // AAACAAAA and i = 7. The idea is similar 
-            // to search step. 
             if (len != 0) { 
-                len = lps[len - 1]; 
-  
-                // Also, note that we do not increment 
-                // i here 
+                len = lps[len - 1];
             } 
-            else // if (len == 0) 
+            else
             { 
                 lps[i] = 0; 
                 i++; 
             } 
         } 
     } 
-} 
+}
   
 // Driver program to test above function 
 int main() 
---
+++ Strings/minCharForPalindromeStr.cpp
@@ -0,0 +1,55 @@
+vector<int> computeLPSArray(string str) 
+{ 
+    int M = str.length(); 
+    vector<int> lps(M); 
+  
+    int len = 0; 
+    lps[0] = 0; // lps[0] is always 0 
+    int i = 1; 
+
+    while (i < M) 
+    { 
+        if (str[i] == str[len]) 
+        { 
+            len++; 
+            lps[i] = len; 
+            i++; 
+        } 
+        else
+        { 
+            if (len != 0) 
+            { 
+                len = lps[len-1]; 
+            } 
+            else
+            { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+    return lps; 
+} 
+  
+// Method returns minimum character to be added at 
+// front to make string palindrome 
+int getMinCharToAddedToMakeStringPalin(string str) 
+{ 
+    string revStr = str; 
+    reverse(revStr.begin(), revStr.end()); 
+  
+    // Get concatenation of string, special character 
+    // and reverse string 
+    string concat = str + "$" + revStr; 
+  
+    //  Get LPS array of this concatenated string 
+    vector<int> lps = computeLPSArray(concat); 
+  
+    // By subtracting last entry of lps vector from 
+    // string length, we will get our result 
+    return (str.length() - lps.back()); 
+} 
+
+int Solution::solve(string A) {
+    return getMinCharToAddedToMakeStringPalin(A);
+}
