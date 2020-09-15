21e452454903182136ada0720d6eaa17b93fe074
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-09-15 23:52:36 +0530

    string multiplication

---
+++ Strings/multiplyStrings.cpp
@@ -0,0 +1,74 @@
+string Solution::multiply(string A, string B) {
+    string num1 = A;
+    string num2 = B;
+    int len1 = num1.size(); 
+    int len2 = num2.size(); 
+    if (len1 == 0 || len2 == 0) 
+    return "0"; 
+  
+    // will keep the result number in vector 
+    // in reverse order 
+    vector<int> result(len1 + len2, 0); 
+  
+    // Below two indexes are used to find positions 
+    // in result.  
+    int i_n1 = 0;  
+    int i_n2 = 0;  
+      
+    // Go from right to left in num1 
+    for (int i=len1-1; i>=0; i--) 
+    { 
+        int carry = 0; 
+        int n1 = num1[i] - '0'; 
+  
+        // To shift position to left after every 
+        // multiplication of a digit in num2 
+        i_n2 = 0;  
+          
+        // Go from right to left in num2              
+        for (int j=len2-1; j>=0; j--) 
+        { 
+            // Take current digit of second number 
+            int n2 = num2[j] - '0'; 
+  
+            // Multiply with current digit of first number 
+            // and add result to previously stored result 
+            // at current position.  
+            int sum = n1*n2 + result[i_n1 + i_n2] + carry; 
+  
+            // Carry for next iteration 
+            carry = sum/10; 
+  
+            // Store result 
+            result[i_n1 + i_n2] = sum % 10; 
+  
+            i_n2++; 
+        } 
+  
+        // store carry in next cell 
+        if (carry > 0) 
+            result[i_n1 + i_n2] += carry; 
+  
+        // To shift position to left after every 
+        // multiplication of a digit in num1. 
+        i_n1++; 
+    } 
+  
+    // ignore '0's from the right 
+    int i = result.size() - 1; 
+    while (i>=0 && result[i] == 0) 
+    i--; 
+  
+    // If all were '0's - means either both or 
+    // one of num1 or num2 were '0' 
+    if (i == -1) 
+    return "0"; 
+  
+    // generate the result string 
+    string s = ""; 
+      
+    while (i >= 0) 
+        s += to_string(result[i--]); 
+  
+    return s; 
+}
