6bb827e4c1cb5c6c3017e2ec244a0918c934fa5f
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-11-28 22:58:00 +0530

    Memoize with map

--- Dynamic Programming/booleanParenthization.cpp
+++ Dynamic Programming/booleanParenthization.cpp
@@ -64,3 +64,78 @@ int main() {
     cin >> s;
     cout<< solve(s, 0, s.size()-1, true);
 }
+
+// Memoization with map
+
+unordered_map<string, int> mp;
+
+int solve(string s, int i, int j, bool isTrue) {
+    if(i > j) {
+        return false;
+    }
+
+    if(i == j) {
+        if(isTrue == true) {
+            return s[i] == 'T';
+        }
+        else {
+            return s[i] == 'F';
+        }
+    }
+
+    string temp = to_string(i);
+    temp.push_back(' ');
+    temp.append(to_string(j));
+    temp.push_back(' ');
+    temp.append(to_string(isTrue));
+
+    if(mp.find(temp) != mp.end()) {
+        return mp[temp];
+    }
+
+    int ans = 0;
+
+    for(int k = i+1; k <= j-1; k+=2) {
+        int lt = solve(s, i, k-1, true);
+        int lf = solve(s, i, k-1, false);
+        int rt = solve(s, k+1, j, true);
+        int rf = solve(s, k+1, j, false);
+
+        if(s[k] == '&') {
+            if(isTrue == true) {
+                ans = ans + lt*rt;
+            }
+            else {
+                ans = ans + lt*rf + lf*rt + lf*rf;
+            }
+        }
+
+        else if(s[k] == '|') {
+            if(isTrue == true) {
+                ans = ans + lt*rt + lt*rf + lf*rt;
+            }
+            else {
+                ans = ans + lf*rf;
+            }
+        }
+
+        else if(s[k] == '^') {
+            if(isTrue == true) {
+                ans = ans + lt*rf + lf*rt;
+            }
+            else {
+                ans = ans + lf*rf + lt*rt;
+            }
+        }
+    }
+
+    return mp[temp] = ans;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    mp.clear();
+    cout<< solve(s, 0, s.size()-1, true);
+}
+
